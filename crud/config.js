8dfb887dee65b98a465e6fac8b48a2cae167cd53
fabien <fabiendubois2304@gmail.com>
2018-04-12 15:27:03 +0200

    maj config

--- crud/config.js
+++ crud/config.js
@@ -1,13 +1,13 @@
 var config = {
     database: {
-        host: '51.254.216.9',     // database host
+        host: '127.0.0.1',     // database host
         user: 'root',         // your database username
         password: 'password',         // your database password
         port: 32770,         // default MySQL port
         db: 'kahlougaroo'         // your database name
     },
     server: {
-        host: '127.0.0.1',
+        host: '51.254.216.9',
         port: 8080
     }
 }
