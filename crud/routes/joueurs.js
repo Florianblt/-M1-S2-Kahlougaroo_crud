fa370fc17e419684613f13b7366bd9c4ec3af58b
fabien <fabiendubois2304@gmail.com>
2018-05-04 10:49:07 +0200

    Add GET /joueurs/:partie/:pseudo

--- crud/routes/joueurs.js
+++ crud/routes/joueurs.js
@@ -30,6 +30,42 @@ router.get('/', function (req, res) {
     });
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// GET /joueurs/:partie/:pseudo
+////////////////////////////////////////////////////////////////////////////////
+router.get('/:partie/:pseudo', function (req, res) {
+    req.getConnection(function (err, connection) {
+        var query = connection.query(
+            `
+            SELECT joueur.token 
+            FROM joueur 
+            INNER JOIN partie ON joueur.partie = partie.token
+            WHERE joueur.pseudo = "${req.params.pseudo}" AND partie.pin = ${req.params.partie}
+            `, function (err, rows) {
+            if (err) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.writeHead(500, { "Content-Type": "application/json" });
+                var result = {
+                    success: false
+                }
+                res.write(JSON.stringify(err));
+                res.end();
+            }
+            else {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.writeHead(200, { "Content-Type": "application/json" });
+                var result = {
+                    success: true,
+                    rows: rows.length,
+                }
+                res.write(JSON.stringify(rows));
+                res.end();
+            }
+        });
+    });
+});
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // POST /joueurs
