894a770db74ced3e3d53596375aa7f496f3cb8b6
Unknown <Florianblot059@gmail.com>
2018-05-08 21:12:02 +0200

    Add : Get /joueurs/:partie

--- crud/routes/joueurs.js
+++ crud/routes/joueurs.js
@@ -66,6 +66,42 @@ router.get('/:partie/:pseudo', function (req, res) {
     });
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// GET /joueurs/:partie
+////////////////////////////////////////////////////////////////////////////////
+router.get('/:partie', function (req, res) {
+    req.getConnection(function (err, connection) {
+        var query = connection.query(
+            `
+            SELECT joueur.token 
+            FROM joueur 
+            INNER JOIN partie ON joueur.partie = partie.token
+            WHERE joueur.master = 0
+            AND partie.pin = ${req.params.partie}
+            `, function (err, rows) {
+                if (err) {
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.writeHead(500, { "Content-Type": "application/json" });
+                    var result = {
+                        success: false
+                    }
+                    res.write(JSON.stringify(err));
+                    res.end();
+                }
+                else {
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    var result = {
+                        success: true,
+                        rows: rows.length,
+                    }
+                    res.write(JSON.stringify(rows));
+                    res.end();
+                }
+            });
+    });
+});
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
